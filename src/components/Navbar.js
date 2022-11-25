6c8a1fbc4ccfedef56f8ebed35ca4c5e5f264133
Muhammad Najib Bala <45116287+NAJIB-B@users.noreply.github.com>
2022-11-25 22:14:03 +0100

    Fixed navbar button (take me to quiz)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => {
   return (
     <div className="bg-blue-600 text-white p-3 font-bold flex flex-row justify-between md:justify-around items-center">
       <Link to="/" className='text-2xl'>ADHD Quiz</Link>
-      <button className="rounded p-3 px-10 md:px-3 bg-white text-blue-600 md:w-1/4">
-        <Link to="/quiz">Take Quiz</Link>
-      </button>
+      
+        <Link to="/quiz" className="rounded p-3 px-10 md:px-3 bg-white text-blue-600 md:w-1/4 text-center" >Take Quiz</Link>
+      
     </div>
   );
 }
