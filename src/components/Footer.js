0f4d62cb7d6dcd62af7b7ed954ebc28bbf0963a8
Ismaila Azeez Oluwatobi <azeezthegenius@gmail.com>
2022-11-25 13:35:25 +0100

    fixed UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <div className="flex flex-col justify-center bg-blue-600 text-white p-4 gap-2 items-center">
+      <span>Built for HNGi9 by Zyzman & Najib</span>
+      <span>&copy; Copyright 2022
+      </span>
+    </div>
+  );
 }
 
 export default Footer
